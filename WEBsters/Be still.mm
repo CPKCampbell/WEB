af92c2a071f51fd72c252d1afad764b0d99dcad5
Colin Campbell <colinpkcampbell@gmail.com>
2016-01-15 19:39:20 -0700

    Reorganization continued

---
+++ WEBsters/Be still.mm
@@ -0,0 +1,177 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1337446346447" ID="ID_944127177" MODIFIED="1337557653847" TEXT="Be still">
+<font NAME="SansSerif" SIZE="20"/>
+<node COLOR="#0033ff" CREATED="1337446383992" HGAP="10" ID="ID_1713167309" MODIFIED="1337557635346" POSITION="right" VSHIFT="-115">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      See the salvation:
+    </p>
+    <p>
+      <i><font size="4">Exodus 14:13</font></i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<cloud/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1337447111585" ID="ID_1968857685" MODIFIED="1337448414182" TEXT="Isrealites not trusting">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1337447145935" ID="ID_338179480" MODIFIED="1337448414183" TEXT="God sometimes leads us out of familiar places">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1337448453978" ID="ID_800411325" MODIFIED="1337448481691" TEXT="He leads us">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1337448497960" ID="ID_32921885" MODIFIED="1337448574485" TEXT="We see His plan if we look for it">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1337448530544" ID="ID_190574832" MODIFIED="1337448538933" TEXT="We let go of the past">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1337447248287" ID="ID_1515788424" MODIFIED="1337478081615" TEXT="Look ahead to salvation, not dwell in the past">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1337447312263" ID="ID_970745756" MODIFIED="1337448414187" TEXT="Baptism draws a line">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#edebe1" COLOR="#00b439" CREATED="1337448678984" ID="ID_21971066" MODIFIED="1337485861272" TEXT="I won&apos;t Turn Back">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="knotify"/>
+</node>
+<node COLOR="#00b439" CREATED="1337485328107" ID="ID_667053346" MODIFIED="1337485514503" TEXT="Run, Run, Run">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="knotify"/>
+</node>
+<node BACKGROUND_COLOR="#b3d5da" COLOR="#00b439" CREATED="1337449269992" ID="ID_897461940" MODIFIED="1337485953095" TEXT="O Happy Day">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="knotify"/>
+</node>
+<node BACKGROUND_COLOR="#b3d5da" COLOR="#00b439" CREATED="1337485348555" ID="ID_1461297178" MODIFIED="1337490368856" TEXT="Shall We Gather at The River">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="knotify"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1337557467032" HGAP="95" ID="ID_955697331" MODIFIED="1337557675432" POSITION="right" VSHIFT="-69">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Be still and know:
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;<i><font size="4">Psalms 46:10</font></i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<cloud/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1337557558804" ID="ID_1922394593" MODIFIED="1337557578757" TEXT="Be Still, My Soul">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="knotify"/>
+</node>
+<node COLOR="#00b439" CREATED="1337557583412" ID="ID_822159717" MODIFIED="1337557607501" TEXT="Sweet Hour of Prayer">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="knotify"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1337446460806" HGAP="114" ID="ID_1197652468" MODIFIED="1337557630503" POSITION="right" VSHIFT="-3">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Consider the wondrous works:
+    </p>
+    <p>
+      <i><font size="4">Job 37:14</font></i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<cloud/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1337446863907" ID="ID_1690120671" MODIFIED="1337448414191" TEXT="Quiet in the presence of God">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1337446888751" ID="ID_168668991" MODIFIED="1337448414192" TEXT="Listen to the priests">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1337448690896" HGAP="22" ID="ID_858527781" MODIFIED="1337557293767" TEXT="He Leadeth Me" VSHIFT="-4">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="knotify"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1337446754951" HGAP="-16" ID="ID_230539506" MODIFIED="1337556917047" POSITION="right" VSHIFT="104">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Peace, be still:
+    </p>
+    <p>
+      <i><font size="4">Mark 4:39</font></i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<cloud/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1337446936539" ID="ID_579412670" MODIFIED="1337448414193" TEXT="Christ calms the waters">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1337446961583" ID="ID_1496474069" MODIFIED="1337448414194" TEXT="His ship is under His protection">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#b3d5da" COLOR="#00b439" CREATED="1337449110016" ID="ID_683181066" MODIFIED="1337529581637" TEXT="Lord, Listen to Your Children Praying">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="knotify"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1337448664656" ID="ID_804291823" MODIFIED="1337557323352" POSITION="left" TEXT="Potential songs">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1337448721168" ID="ID_1872620290" MODIFIED="1337449319389" TEXT="I&apos;ve Been Changed">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="knotify"/>
+</node>
+<node COLOR="#00b439" CREATED="1337557334572" ID="ID_1129193877" MODIFIED="1337557346126" TEXT="Steal Away">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="knotify"/>
+</node>
+</node>
+</node>
+</map>
